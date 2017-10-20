a5e1daaad2e76cf0bb9b4c75c90b43f8ccee39f4
BrahimYacine <fy_brahim@esi.dz>
2017-10-20 18:00:34 +0000

    Ajout de l'attribut vector et Creation de getVector et SetVector

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Brahim on 20/10/2017.
  */
 public class VectorHelper {
-    
+    private List<Flaot> vector = new ArrayList<Float>();
+
+    public List<Flaot> getVector() {
+        return vector;
+    }
+
+    public void setVector(List<Flaot> vector) {
+        this.vector = vector;
+    }
+
 }
