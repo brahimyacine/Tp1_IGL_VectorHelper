b782ee8a7f6c8035f4153f88aacfc9bb91f9f6dc
abd0u <fa_djellal@esi.dz>
2017-10-24 20:39:19 +0200

    dding the javadoc to the Class

--- VectorHelper.java
+++ VectorHelper.java
@@ -2,27 +2,64 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
+/**
+ * this is a class that gives some methods to help manupulating a vector giving by the
+ * user , the user need to enter a string with the numbers that he wants to be inside his
+ * ARRAY then he chooses what he want to do with it
+ * @author Djellal_and_Brahim
+ * @version final
+ */
 public class VectorHelper {
+    /**
+     * this is the atribute vector that is filled by the user
+     */
     private List <Float> vector = new ArrayList<>();
 
+    /**
+     * the constructor of the class VectorHelper
+     * @param vector which is used to fill the atribute victor
+     */
     public VectorHelper(List<Float> vector) {
         for(int i=0;i<vector.size();i++)
 
             this.vector.add(vector.get(i));
 
     }
+
+    /**
+     * the constructor with no arguments for the class victorHelper
+     */
     public VectorHelper() {
 
     }
+
+    /**
+     * return the victor that will be used by the user
+     * he  can manipulate it with the help  of the methodes below
+     * @return the victor after filling it by the user
+     */
     public List<Float> getVector() {
         return vector;
     }
+    /**
+     * the setter of the atribute vector
+     * this methode is used if the user want to change the existing
+     * Array in the atribute with another one using the parameter vector
+     * @param vector ' the atribute '
+     * @since the first
+     */
 
     public void setVector(List<Float> vector) {
         this.vector = vector;
     }
 
+    /**
+     * this methode sort the victor in the atribute filed
+     * it used the principle of "trie a bull "
+     * so the Array will be sorted from the little to the biggest
+     * @author Brahim
+     * @since the first
+     */
     public void sortVerctor()
     {
         boolean bool = true ;
@@ -49,6 +86,11 @@ public class VectorHelper {
 
         }
     }
+
+    /**
+     *
+     * @return the bigest element in the atribute victor which is a Float
+     */
     public float getMax()
     {
         float max  = vector.get(0) ;
@@ -58,6 +100,11 @@ public class VectorHelper {
                 max = vector.get(i) ;
         return max;
     }
+
+    /**
+     *
+     * @return the smallest element in the atribute victor which is a Float
+     */
     public float getMin()
     {
 
@@ -69,6 +116,13 @@ public class VectorHelper {
         return min;
 
     }
+
+    /**
+     * this methode is used to add two victors
+     * @param vector1 the second victor to add because the first is the atribute
+     * @return the resulting victor == the addition between the 2 victors
+     * @throws SizeException if the sizes of the victors are different
+     */
     public List<Float> addVectors(List<Float> vector1)throws SizeException
     {   int size = vector.size();
         if(vector.size()!=vector1.size()) throw new SizeException();
@@ -84,6 +138,11 @@ public class VectorHelper {
     }
 
 
+    /**
+     * invert the elements of the atribute vector
+     * @since the second
+     * @author DJellal
+     */
     public void Invert () // to invert the victor
     {
         int j=vector.size()-1 ;
@@ -99,6 +158,10 @@ public class VectorHelper {
         }
     }
 
+    /**
+     * multiplaying all the elements of the victor by the parametere k wich is an integer
+     * @param k an integer
+     */
     public void Multiplication ( int k) //u chose the number
     {
         for (int i=0;i<vector.size();i++)
@@ -107,6 +170,12 @@ public class VectorHelper {
         }
     }
 
+    /**
+     * this methode is used to fill the atribute victor with Floats elements
+     * the principle is to transform the numbers in the text arguments which is a string
+     * to numbers and then fill the ARRAY (parsing == transforming to )
+     * @param text
+     */
     public void parseText (String text)
     {
         String [] tab = text.split (" ");
@@ -119,6 +188,10 @@ public class VectorHelper {
         }
     }
 
+    /**
+     * fill the atribute victor with the elements inside the parameter vector
+     * @param vector given by the user
+     */
     public void fillVector(List<Float> vector)
     {
         this.vector.clear();
@@ -128,18 +201,31 @@ public class VectorHelper {
 
 
     }
+
+    /**
+     *
+     * @param a first integer to add
+     * @param b second integer to add
+     * @return the addition of parametere 1 and 2
+     */
     public int add(int a,int b)
     {
         return a+b;
     }
 
 
-
+    /**
+     * this methode is used to print a vector which containes Floats Elements
+     * into the consol log by transforming the victor to a string
+     */
     public void printVector()
     {
         System.out.println(vector.toString());
     }
 
+    /**
+     * this is the main methode which is used to run the prgrm who use the class and it'smethode below
+     */
     public static void main(String[] args) {
         /*Scanner sc=new Scanner(System.in);
         VectorHelper Vcc1 = new VectorHelper();
